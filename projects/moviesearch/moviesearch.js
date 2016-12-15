9a718d98b3cb85f98876247f93b82c501d6bac8d
Allan Davies <aloz@hotmail.com.au>
2016-12-15 17:16:04 +1100

    fixed another mixed content

--- projects/moviesearch/moviesearch.js
+++ projects/moviesearch/moviesearch.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 $(".searchButton").on("click",function() {
     
-    var url="http://www.omdbapi.com/?&plot=full&tomatoes=true&t="+$(".searchField").val()+"&callback=?"
+    var url="https://www.omdbapi.com/?&plot=full&tomatoes=true&t="+$(".searchField").val()+"&callback=?"
     $.getJSON(url,function(data) {
        $(".result").html('<div class="title">'+data.Title+'</div><div class="poster"><a class="weblink" href="#" target="_blank"><img src="'+data.Poster+'"/></a></div><div class="info">'+data.Plot+'</br></br>Director: '+data.Director+'</br>Actors: '+data.Actors+'</div><div class="facts"><div class="misc"><h3>Facts</h3>Genre: '+data.Genre+'</br>Country: '+data.Country+'</br>Awards: '+data.Awards+'</div><div class="ratings"><h3>Ratings</h3>Imdb rating: '+data.imdbRating+'</br>Tomato rating: '+data.tomatoRating+'</br>Tomato user rating: '+data.tomatoUserRating+'</div>');
     
@@ -16,7 +16,7 @@ $(".searchButton").on("click",function() {
 
 $(".searchField").on("keypress",function(event) {
     if(event.which ==13) {
-        var url="http://www.omdbapi.com/?&plot=full&tomatoes=true&t="+$(".searchField").val()+"&callback=?"
+        var url="https://www.omdbapi.com/?&plot=full&tomatoes=true&t="+$(".searchField").val()+"&callback=?"
     $.getJSON(url,function(data) {
        $(".result").html('<div class="title">'+data.Title+'</div><div class="poster"><a class="weblink" href="#" target="_blank"><img src="'+data.Poster+'"/></a></div><div class="info">'+data.Plot+'</br></br>Director: '+data.Director+'</br>Actors: '+data.Actors+'</div><div class="facts"><div class="misc"><h3>Facts</h3>Genre: '+data.Genre+'</br>Country: '+data.Country+'</br>Awards: '+data.Awards+'</div><div class="ratings"><h3>Ratings</h3>Imdb rating: '+data.imdbRating+'</br>Tomato rating: '+data.tomatoRating+'</br>Tomato user rating: '+data.tomatoUserRating+'</div>');
     
